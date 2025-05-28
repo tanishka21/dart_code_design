18b5dc62081930d07c6954844082b7ae996130d4
tanishka21 <tanishkagoel7728@gmail.com>
2025-05-29 01:24:49 +0530

    fibonacci sequence

--- fibonacci_sequence.dart
+++ fibonacci_sequence.dart
@@ -1,1 +1,26 @@
-void main() {}
+void main() {
+  /// 0,1,1,2,3,5,8....
+  /// First Approach
+  int count = 10;
+  int first = 0, second = 1, next;
+  for (int i = 0; i < count; i++) {
+    if (i <= 1) {
+      next = i;
+    } else {
+      next = first + second;
+      first = second;
+      second = next;
+    }
+    print(next);
+  }
+
+  /// Second Approach
+  int n = 10;
+  List<int> fib = [0, 1];
+  // var next;
+  for (int i = 2; i < n; i++) {
+    int next = fib[i - 1] + fib[i - 2];
+    fib.add(next);
+  }
+  print(fib);
+}
