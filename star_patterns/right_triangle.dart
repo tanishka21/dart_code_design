11f71afde7f676814937f62153393a30deb375ba
tanishka21 <tanishkagoel7728@gmail.com>
2025-05-31 00:55:57 +0530

    basic pattern

--- star_patterns/right_triangle.dart
+++ star_patterns/right_triangle.dart
@@ -8,6 +8,6 @@ void main() {
    */
   int n = 5;
   for (int i = 0; i <= n; i++) {
-    print("*" * i);
+    print("* " * i);
   }
 }
---
+++ star_patterns/star_.dart
@@ -0,0 +1,16 @@
+void main() {
+  /*
+  * * * *
+  * * * *
+  * * * *
+  * * * *
+  */
+  int n = 5;
+  for (int i = 0; i < n; i++) {
+    String line = "";
+    for (int j = 0; j < n; j++) {
+      line += "* ";
+    }
+    print(line);
+  }
+}
